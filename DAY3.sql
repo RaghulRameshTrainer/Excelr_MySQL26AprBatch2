b8c12937a65b2cd473eeb73b26695a669a799c14
RaghulRameshTrainer <89928567+RaghulRameshTrainer@users.noreply.github.com>
2022-04-28 22:22:34 +0530

    DAY3

--- DAY3.sql
+++ DAY3.sql
@@ -70,4 +70,55 @@ SELECT * FROM unique_notnull_table;
 
 INSERT INTO unique_notnull_table(custid,custname,city) VALUES(101,'Aveek Mishra','Kolkatta');
 
+SELECT count(*) FROM unique_notnull_table;
+
+
+CREATE TABLE default_const_tbl(
+custid INT UNIQUE NOT NULL,
+custname VARCHAR(100) NOT NULL,
+city VARCHAR(100) DEFAULT 'Hyderabad'
+);
+
+INSERT INTO default_const_tbl(custid,custname) VALUES(1000,'Rajesh Kuamr');
+SELECT * FROM default_const_tbl;
+INSERT INTO default_const_tbl(custid,custname,city) VALUES(1001,'Ashwin Sekar','Chennai');
+
+-- CHECK CONSTAINTS
+
+CREATE TABLE check_const_tbl(
+custid INT UNIQUE NOT NULL,
+custname VARCHAR(100) NOT NULL,
+city VARCHAR(100) DEFAULT 'Hyderabad',
+age INT CHECK (age >=0 AND age<=100)
+);
+
+INSERT INTO check_const_tbl VALUES(100,'Ravi Kuamr','Bangalore',22);
+
+SELECT * FROM check_const_tbl;
+
+CREATE TABLE check_const_tbl2(
+custid INT UNIQUE NOT NULL,
+custname VARCHAR(100) NOT NULL,
+city VARCHAR(100) DEFAULT 'Hyderabad',
+age INT ,
+country VARCHAR(100) CHECK ( country IN ('INDIA','CANADA','USA','AUSTRALIA'))
+);
+
+
+INSERT INTO check_const_tbl2 VALUES(100,'Ramamoorthy Kandasamy','Chennai',32,'INDIA');
+
+SELECT * FROM check_const_tbl2;
+
+INSERT INTO check_const_tbl2 VALUES(101,'Ramamoorthy Kandasamy','Chennai',32,'Canada');
+
+
+
+
+CREATE TABLE check_const_tbl2(
+custid INT UNIQUE NOT NULL,
+custname VARCHAR(100) NOT NULL,
+city VARCHAR(100) DEFAULT 'Hyderabad',
+age INT ,
+country VARCHAR(100) CHECK ( country='INDIA' OR country='CANADA' OR country='USA' OR country='AUSTRALIA')
+);
 
