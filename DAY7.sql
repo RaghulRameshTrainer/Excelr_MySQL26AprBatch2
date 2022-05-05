0bafe6b8a29ae8aeff02707afde1f4e2138c83a7
RaghulRameshTrainer <89928567+RaghulRameshTrainer@users.noreply.github.com>
2022-05-05 22:10:29 +0530

    DAY7

--- DAY7.sql
+++ DAY7.sql
@@ -35,7 +35,7 @@ VALUES(1,'Kolanchi',current_timestamp()),
 
 SELECT * FROM date_function;
 
-SELECT id,name,date_format(registered_date,'%W %D %M %Y %h:%i:%s %p') FROM date_function;
+SELECT id,name,date_format(registered_date,'%W %D %M %Y %h:%i:%s %p') as joined_date FROM date_function;
 
 /*
 05/05/2022
@@ -47,3 +47,60 @@ Thu May 05 2022
 12:30 PM May 05 2022
 May 05 2022
 */
+
+-- TCL STATEMENTS => COMMIT ROLLBACK SAVEPOINT
+
+SELECT * FROM ORDER_TBL;
+
+START TRANSACTION;
+UPDATE ORDER_TBL SET amount =1 WHERE amount IS NULL;
+COMMIT;
+ROLLBACK;
+SET sql_safe_updates=0;
+
+/*
+Mobile 25000 -> 30000
+Laptop 70000 -> 100000
+Charger 1500 -> 2500
+Tab  20000 -> 25000
+Desktop 15000 -> 20000
+*/
+
+START TRANSACTION ;
+
+SAVEPOINT mobile_trans2;
+UPDATE ORDER_TBL SET amount=20000 WHERE product_name='Mobile';
+
+SAVEPOINT laptop_update2;
+UPDATE ORDER_TBL SET amount=70000 WHERE product_name='Laptop';
+
+SAVEPOINT charger_update2;
+UPDATE ORDER_TBL SET amount=1500 WHERE product_name='Charger';
+
+SAVEPOINT Tab_update2;
+UPDATE ORDER_TBL SET amount=20000 WHERE product_name='Tab';
+
+SAVEPOINT Desktop_update2;
+UPDATE ORDER_TBL SET amount=15000 WHERE product_name='Desktop';
+
+SELECT * FROM ORDER_TBL;
+
+ROLLBACK TO charger_update2;
+
+COMMIT;
+
+
+-- ROUND FUNCTION
+
+SELECT ROUND(99.5) AS amount;
+SELECT ROUND(-99.25) AS amount;
+
+SELECT ROUND(12121.1246673,3) as amount;
+
+SELECT ROUND(12121.1246673,-3) as amount;
+SELECT ROUND(12621.1246673,-3) as amount;
+
+-- SUBSTR FUNCTION
+
+SELECT SUBSTR("EXCELR",3) AS company;
+SELECT SUBSTR("EXCELR",1,5) AS company;
